ca16014e44f6500ed354177c9a16dd1230e564f5
Rati Jebisashvili <rati.jebisashvili.1@iliauni.edu.ge>
2022-01-31 01:01:05 +0400

    added back steps

--- app/javascript/controllers/slots_controller.js
+++ app/javascript/controllers/slots_controller.js
@@ -2,57 +2,95 @@ import { Controller } from "stimulus"
 import Rails from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = [ "country", "city", "district", "bu_unit", "slots", "refresh" ]
+  static targets = [
+    "country",
+    "city",
+    "district",
+    "bu_unit",
+    "slots",
+    "refresh"
+  ]
+
+  static values = {
+        countryId: String,
+        cityId: String,
+        districtId: String,
+        buUnitId: String,
+        orderDate: String
+  }
 
   connect() {
-    console.log("Hi");
+    if(this.buUnitIdValue) {
+      this.fetchCities()
+      this.fetchDistricts()
+      this.fetchBusinessUnits()
+      this.fetchSlots()
+    }
   }
 
   fetchCities(){
+    let country_id = this.countryTarget.value || this.countryIdValue
     Rails.ajax({
       type: "GET",
       url: "/slots/fetch_cities",
-      data: "country_id=" + this.countryTarget.value,
+      data: "country_id=" + country_id + "&dt=" + this.countryIdValue,
       success: (data) => {
-          this.cityTarget.innerHTML = data.body.innerHTML;
+          this.cityTarget.innerHTML = data.body.innerHTML
       }
     });
   }
 
   fetchDistricts(){
+    let city_id = this.cityTarget.value || this.cityIdValue
+
     Rails.ajax({
-      type: "GET",
-      url: "/slots/fetch_districts",
-      data: "city_id=" + this.cityTarget.value,
-      success: (data) => {
-          this.districtTarget.innerHTML = data.body.innerHTML;
-      }
+        type: "GET",
+        url: "/slots/fetch_districts",
+        data: "city_id=" + city_id + "&dt=" + this.cityIdValue,
+        success: (data) => {
+            this.districtTarget.innerHTML = data.body.innerHTML
+        }
     });
   }
   
   fetchBusinessUnits(){
+    let country_id = this.countryTarget.value || this.countryIdValue
+    let city_id = this.cityTarget.value || this.cityIdValue
+    let district_id = this.districtTarget.value || this.districtIdValue
+
     Rails.ajax({
-      type: "GET",
-      url: "/slots/fetch_business_units",
-      data: "country_id=" + this.countryTarget.value + "&city_id=" + this.cityTarget.value + "&district_id=" + this.districtTarget.value,
-      success: (data) => {
-          this.bu_unitTarget.innerHTML = data.body.innerHTML;
-      }
+        type: "GET",
+        url: "/slots/fetch_business_units",
+        data: "country_id=" + country_id + "&city_id=" + city_id + "&district_id=" + district_id + "&dt=" + this.buUnitIdValue,
+        success: (data) => {
+            this.bu_unitTarget.innerHTML = data.body.innerHTML
+        }
     });
   }
 
-  fetchSlots(){
+  fetchSlots() {
+    let bu_id = this.bu_unitTarget.value || this.buUnitIdValue
+
     Rails.ajax({
-      type: "GET",
-      url: "/slots/",
-      data: "business_unit_id=" + this.bu_unitTarget.value,
-      success: (data) => {
-          debugger
-          this.slotsTarget.innerHTML = data.body.innerHTML;
+        type: "GET",
+        url: "/slots/",
+        data: "business_unit_id=" + bu_id + "&dt=" + this.orderDateValue,
+        success: (data) => {
+          this.slotsTarget.innerHTML = data.body.innerHTML
+
+          let currentSlot = document.querySelector('.slot-item-selected')
           this.refreshTarget.disabled = false;
-      }
+
+          if(currentSlot) {
+              currentSlot.classList.toggle("btn-warning")
+
+              if(currentSlot.classList.contains('btn-warning')) {
+                  document.querySelector('input.step1_submit').classList.remove('invisible')
+              }
+          }
+        }
     });
-  }
+}
 
   selectSlot(){
     Array.from(document.querySelectorAll('.slot-item')).forEach(function(el){
